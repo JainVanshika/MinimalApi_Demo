2738ec86df61a44e1143ebcdef852622eb103448
vanshika <user@DESKTOP-LSEDR8U>
2023-04-24 03:20:03 +0530

    Add project files.

---
+++ MagicVilla_CouponAPI/Models/DTO/CouponCreateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace MagicVilla_CouponAPI.Models.DTO
+{
+    public class CouponCreateDTO
+    {
+        //it is telling what data user can add
+        public string Name { get; set; }
+        public int Percent { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
